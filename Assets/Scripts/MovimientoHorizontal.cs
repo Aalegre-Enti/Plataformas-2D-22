52f12715e6d016430ea860b8449d73825a5b46fe
Enti <licenses@enti.cat>
2022-10-14 14:25:00 +0200

    Animaciones

--- Assets/Scripts/MovimientoHorizontal.cs
+++ Assets/Scripts/MovimientoHorizontal.cs
@@ -4,17 +4,41 @@ using UnityEngine;
 
 public class MovimientoHorizontal : MonoBehaviour
 {
-    public float velocidad;
+    public float velocidad_grounded;
+    public float velocidad_air;
+    Animator anim;
+    GroundDetector ground;
     // Start is called before the first frame update
     void Start()
     {
-        
+        ground = GetComponent<GroundDetector>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
-        transform.position = transform.position + new Vector3(horizontal * velocidad * Time.deltaTime, 0, 0);
+
+        anim.SetBool("grounded", ground.grounded);
+        anim.SetBool("moving", horizontal != 0);
+
+        if(ground.grounded == true)
+        {
+            horizontal = horizontal * velocidad_grounded;
+        }
+        else
+        {
+            horizontal = horizontal * velocidad_air;
+        }
+        if(horizontal > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        if(horizontal < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        transform.position = transform.position + new Vector3(horizontal * Time.deltaTime, 0, 0);
     }
 }
